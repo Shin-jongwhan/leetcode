ae2d81efe2bccd878510bd14820a0e4b586a138e
Shin-jonghwan <62974484+Shin-jongwhan@users.noreply.github.com>
2024-10-25 16:02:24 +0900

    Update answer.py

--- Top_interview_question/6_Dynamic_Programming/4_House_Robber/answer.py
+++ Top_interview_question/6_Dynamic_Programming/4_House_Robber/answer.py
@@ -4,9 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        dp = [0, nums[0]]
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+        opt = [0] * n
+        opt[0], opt[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2, n):
+            opt[i] = max(nums[i] + opt[i-2], opt[i-1])
 
-        for idx, num in enumerate(nums[1:]):
-            dp.append(max(dp[idx]+num, dp[idx+1]))
-
-        return dp[-1]
+        return opt[n-1]
